fb51ada7b1a0751e196547187fa1ff31a2e6610a
naelem <ellenxxy19@gmail.com>
2025-04-30 16:35:14 -0300

    [FEA] - Browser Router configured

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import './App.module.css'
 import './global.css'
+import { BrowserRouter } from 'react-router-dom'
+import Router from './Routes'
 import Header from './components/header/Header'
 import Home from './pages/home/Home'
 
 export default function App() {
 
   return (
-    <>
-      <Header />
-      <Home />
-    </>
+    <BrowserRouter>
+      <Router />
+    </BrowserRouter>
   )
 }
 
