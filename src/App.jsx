d343c654515d1dec12ef4a1b804f98c2671f29bc
naelem <ellenxxy19@gmail.com>
2025-05-01 16:17:02 -0300

    Cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,8 @@ import './App.module.css'
 import './global.css'
 import { BrowserRouter } from 'react-router-dom'
 import Router from './Routes'
-import Header from './components/header/Header'
-import Home from './pages/home/Home'
 
 export default function App() {
-
   return (
     <BrowserRouter>
       <Router />
