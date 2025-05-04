5ad8a0f184d44b13856005049458352512625199
naelem <ellenxxy19@gmail.com>
2025-05-03 21:54:03 -0300

    [FEAT] - style is finished

--- src/pages/order-confirmation/OrderConfirmation.jsx
+++ src/pages/order-confirmation/OrderConfirmation.jsx
@@ -12,24 +12,24 @@ export default function OrderConfirmation() {
                 
                 <section className={styles.orderInfo}>
                     <div className={styles.infoDelivery}>
-                        <MapPin size={20} />
+                        <MapPin className={styles.iconMap} size={20} />
                         <div>
-                            <p>Entrega em Rua João Daniel Martinelli, 102</p>
+                            <p>Entrega em Rua <strong>João Daniel Martinelli</strong>, 102</p>
                             <span>Alvorada - Amazonas, AM</span>
                         </div>
                     </div>
                     <div className={styles.infoTimePrediction}>
-                        <Timer size={20} />
+                        <Timer className={styles.iconTimer} size={20} />
                         <div>
                             <p>Previsão de entrega</p>
-                            <span>20 min - 30 min</span>
+                            <span><strong>20 min - 30 min</strong></span>
                         </div>
                     </div>
                     <div className={styles.infoPayment}>
-                        <Money size={20} />
+                        <Money className={styles.iconMoney} size={20} />
                         <div>
                             <p>Pagamento na entrega</p>
-                            <span>Cartão de Débito</span>
+                            <span><strong>Cartão de Débito</strong></span>
                         </div>
                     </div>
                 </section>
